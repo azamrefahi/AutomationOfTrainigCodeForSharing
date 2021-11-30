d05fd2f05a9d1a74009afe8f81e36be49bfbf641
azamr <azamr@DESKTOP-U7L3LH0.myfiosgateway.com>
2021-11-29 19:30:28 -0500

    first time training

---
+++ Automationtraining/src/test/java/PeopleNTech/Automationtraining/Ibrowserworking.java
@@ -0,0 +1,21 @@
+package PeopleNTech.Automationtraining;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ibrowserworking {
+static WebDriver driver;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //i)how to open the browser
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\azamr\\git\\repository\\Automationtraining\\src\\Drivers\\chromedriver.exe");
+		
+
+		
+
+	
+	
+
+	}
+
+}
